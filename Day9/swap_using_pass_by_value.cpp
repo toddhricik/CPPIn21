3415ca35fab11b9bd421c05062c3ec7f48c5f9e2
toddhricik <toddhricik@gmail.com>
2021-07-09 20:54:06 -0400

    Listing 9.5

---
+++ Day9/swap_using_pass_by_value.cpp
@@ -0,0 +1,29 @@
+// Listing 9.5 - Demonstrates passing by value
+#include <iostream>
+
+void swap(int x, int y);
+
+int main()
+{
+	int x=5, y=10;
+
+	std::cout << "Main. Before swap, x: " << x << " y: " << y << "\n";
+	swap(x, y);
+	std::cout << "Main. After swap, x: " << x << " y: " << y << "\n";
+	return 0;
+}
+
+void swap(int x, int y)
+{
+	int temp;
+
+	std::cout << "Swap. Before swap, x: " << x << " y: " << y << "\n";
+	
+	temp = x;
+	x = y;
+	y = temp;
+
+	std::cout << "Swap. After swap, x: " << x << " y: " << y << "\n";
+}
+
+
