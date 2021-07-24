1cbdd269d0ad5836b24d1d7d2e777f7ec19627d4
toddhricik <toddhricik@gmail.com>
2021-07-23 21:35:37 -0400

    Listing 13.4 corrected

--- Day13/array_of_objects.cpp
+++ Day13/array_of_objects.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 class Cat{
 	public:
-		Cat() { itstAge = 1; itsWeight = 5; }
+		Cat() { itsAge = 1; itsWeight = 5; }
 		~Cat() {}
 		int GetAge() const { return itsAge; }
 		int GetWeight() const { return itsWeight; }
